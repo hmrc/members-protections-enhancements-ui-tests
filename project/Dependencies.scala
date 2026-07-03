a34b2867b253fc4ba87891570efcc31babc488c9
davidfes <128475768+davidfes@users.noreply.github.com>
2026-07-03 09:47:55 +0100

    Update runner version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   // format: off
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "ui-test-runner"     % "0.52.0",
+    "uk.gov.hmrc"         %% "ui-test-runner"     % "0.54.0",
     "junit"                % "junit"              % "4.13.2",
   ).map(_ % Test)
   // format: on
